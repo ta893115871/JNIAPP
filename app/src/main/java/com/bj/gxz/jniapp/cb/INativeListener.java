b1646037b37bd2b558b21be558606390cef94fca
guxiuzhong <gfj19900401@126.com>
2021-04-30 11:13:25 +0800

    Update INativeListener.java

--- app/src/main/java/com/bj/gxz/jniapp/cb/INativeListener.java
+++ app/src/main/java/com/bj/gxz/jniapp/cb/INativeListener.java
@@ -1,7 +1,7 @@
 package com.bj.gxz.jniapp.cb;
 
 /**
- * Created by guxiuzhong@baidu.com on 2020/9/21.
+ * Created by guxiuzhong on 2020/9/21.
  */
 public interface INativeListener {
     void onCall();
